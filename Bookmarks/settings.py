0f3242408ccdab626800c593a11b1cb4173e1385
Fenyxz <enmadofenyxz@gmail.com>
2024-10-26 19:55:42 +0000

    Final Touches

--- Bookmarks/settings.py
+++ Bookmarks/settings.py
@@ -102,7 +102,6 @@ WSGI_APPLICATION = "Bookmarks.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-print(os.getenv("DATABASE_URL"))
 
 DATABASES = {
     "default": dj_database_url.config(default=os.getenv("DATABASE_URL")),
