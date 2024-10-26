d00907eb096707f07311b165378078fea2b33e61
Fenyxz <enmadofenyxz@gmail.com>
2024-10-26 19:48:51 +0000

    Changing DB from MySQL to Postgres

--- Bookmarks/settings.py
+++ Bookmarks/settings.py
@@ -102,6 +102,7 @@ WSGI_APPLICATION = "Bookmarks.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+print(os.getenv("DATABASE_URL"))
 
 DATABASES = {
     "default": dj_database_url.config(default=os.getenv("DATABASE_URL")),
