389d0016d4432d56eebe78bef33717e6fb3f219d
Fenyxz <enmadofenyxz@gmail.com>
2024-10-27 04:41:41 +0000

    Updateting settings.py

--- Bookmarks/settings.py
+++ Bookmarks/settings.py
@@ -33,20 +33,14 @@ DEBUG = True
 
 ALLOWED_HOSTS: list = [
     "django-social-website.up.railway.app",
-    # "mysite.com",
-    # "localhost",
-    "127.0.0.1",
-    # "0.0.0.0",
 ]
 
 CORS_ORIGIN_WHITELIST: list = [
     "https://django-social-website.up.railway.app",
-    "https://127.0.0.1:8000",
 ]
 
 CSRF_TRUSTED_ORIGINS: list = [
     "https://django-social-website.up.railway.app",
-    "https://127.0.0.1:8000",
 ]
 
 # Application definition
