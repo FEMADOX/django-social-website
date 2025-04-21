99acdfd4cc341d38acd76d60f4622a182173a5c5
Giancarlos <enmadofenyxz@gmail.com>
2025-04-21 10:03:46 -0400

    Update settings.py

--- Bookmarks/settings.py
+++ Bookmarks/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-+$+4%-emj*37q9^y3i@ky25)xk@*ytxr)76sfh6d+(j)!j7j*0"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS: list = [
     # "django-social-website.up.railway.app",
