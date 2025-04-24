c6e435498d73f9f5661b0d40c936db2d64ee680d
FEMADOX <enmadofenyxz@gmail.com>
2025-04-24 09:26:30 -0400

    Update database configuration and replace psycopg2-binary with psycopg

--- Bookmarks/settings.py
+++ Bookmarks/settings.py
@@ -91,8 +91,7 @@ WSGI_APPLICATION = "Bookmarks.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    # "default": dj_database_url.config(config("DATABASE_URL")),  # type: ignore
-    "default": dj_database_url.config(default="postgresql://postgres:oEDIHbcPjenziFCYIkcjbWWoGUxOGGng@autorack.proxy.rlwy.net:50388/railway"),  # type: ignore
+    "default": dj_database_url.config(default=config("DATABASE_URL")),  # type: ignore
 }
 
 
