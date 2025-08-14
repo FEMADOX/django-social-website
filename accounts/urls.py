bd9b785e3df901d8c50120dc7ff95b35fb9a0c20
FEMADOX <enmadofenyxz@gmail.com>
2025-08-14 11:18:49 -0400

    Refactor URL routing by removing commented-out paths for cleaner code; streamline paginator logic by eliminating unused code segments to enhance readability and maintainability.

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,8 +4,6 @@ from django.urls import include, path
 from accounts import views
 from accounts.forms import CustomPasswordResetForm
 
-# // from django.contrib.auth import views as auth_views
-
 urlpatterns = [
     path("", views.dashboard, name="dashboard"),
     path("", include("django.contrib.auth.urls")),
@@ -25,34 +23,4 @@ urlpatterns = [
         name="account_activation",
     ),
     path("email-validation/", views.email_validation, name="email_validation"),
-    # // path("login/", views.user_login, name="user_login"),
-    # // path("login/", auth_views.LoginView.as_view(), name="login"),
-    # // path("logout/", auth_views.LogoutView.as_view(), name="logout"),
-    # // Password Change
-    # // path(
-    # //     "password-change/",
-    # //     auth_views.PasswordChangeView.as_view(),
-    # //     name="password_change",
-    # // ),
-    # // path(
-    # //     "password-change/done/",
-    # //     auth_views.PasswordChangeDoneView.as_view(),
-    # //     name="password_change_done",
-    # // ),
-    # // Password Reset
-    # // path(
-    # //     "password-reset/done/",
-    # //     auth_views.PasswordResetDoneView.as_view(),
-    # //     name="password_reset_done",
-    # // ),
-    # // path(
-    # //     "password-reset/<uidb64>/<token>/",
-    # //     auth_views.PasswordResetConfirmView.as_view(),
-    # //     name="password_reset_confirm",
-    # // ),
-    # // path(
-    # //     "password-reset/complete",
-    # //     auth_views.PasswordResetCompleteView.as_view(),
-    # //     name="password_reset_complete",
-    # // ),
 ]
